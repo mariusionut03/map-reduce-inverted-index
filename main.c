626e105e82ae33d6938070d87f147d012038711e
mariusionut03 <ionutteq222@gmail.com>
2024-11-27 11:20:21 +0200

    create/join threads

--- main.c
+++ main.c
@@ -15,11 +15,13 @@ typedef struct {
 /* Mapper thread function */
 void *thread_mapper(void *arg) {
     int thread_id = *((int *)arg);
+    printf("Mapper thread with ID=%d.\n", thread_id);
 }
 
 /* Reducer thread function */
 void *thread_reducer(void *arg) {
     int thread_id = *((int *)arg);
+    printf("Reducer thread with ID=%d.\n", thread_id);
 }
 
 int main(int argc, char **argv)
@@ -38,6 +40,31 @@ int main(int argc, char **argv)
     /* Read input file, distribute files */
     int num_mappers = atoi(argv[1]);
     int num_reducers = atoi(argv[2]);
+    int number_of_threads = num_mappers + num_reducers;
     char *input_file = argv[3];
+
+    /* Create threads */
+    pthread_t threads[number_of_threads];
+    int r;
+    long id;
+    void *status;
+    long arguments[number_of_threads];
+
+    for (id = 0; id < number_of_threads; id++) {
+        arguments[id] = id;
+        r = pthread_create(&threads[id], NULL, id < num_mappers ? thread_mapper : thread_reducer, (void *) &arguments[id]);
+        if (r) {
+            printf("Eroare la crearea thread-ului %ld\n", id);
+            exit(-1);
+        }
+    }
+
+    for (id = 0; id < number_of_threads; id++) {
+        r = pthread_join(threads[id], &status);
+        if (r) {
+            printf("Eroare la asteptarea thread-ului %ld\n", id);
+            exit(-1);
+        }
+    }
     return 0;
 }
